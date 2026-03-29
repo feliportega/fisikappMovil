e4d4b10537218c6e273dbfa48853374760d3b291
john salazar <johnfsala@gmail.com>
2026-03-29 18:42:05 -0500

    c

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,9 @@ android {
 
 dependencies {
     implementation(libs.appcompat)
-    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
