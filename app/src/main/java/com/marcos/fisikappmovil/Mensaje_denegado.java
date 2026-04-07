208423d32eb2bba891c242dc2cb4004f8cff8807
john salazar <johnfsala@gmail.com>
2026-04-06 19:36:00 -0500

    despliegue a la rama johnf

---
+++ app/src/main/java/com/marcos/fisikappmovil/Mensaje_denegado.java
@@ -0,0 +1,24 @@
+package com.marcos.fisikappmovil;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Mensaje_denegado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_mensaje_denegado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
