154c16269abe1367493357abef229317f445e8ad
feliportega <felipeortegacarvajal@gmail.com>
2026-04-08 20:59:54 -0500

    primer comit

--- app/src/main/java/com/marcos/fisikappmovil/MainActivity.java
+++ app/src/main/java/com/marcos/fisikappmovil/MainActivity.java
@@ -1,6 +1,9 @@
 package com.marcos.fisikappmovil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btncomienza;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btncomienza = findViewById(R.id.btnComienza);
+        btncomienza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent irLogin = new Intent(MainActivity.this, Login.class);
+                startActivity(irLogin);
+            }
+        });
     }
 }
