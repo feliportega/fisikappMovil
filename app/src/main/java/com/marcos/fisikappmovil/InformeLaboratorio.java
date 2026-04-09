5fe3725b82cb6eb5491ba1cf3824ec6eba3fea86
feliportega <felipeortegacarvajal@gmail.com>
2026-04-08 21:23:47 -0500

    se elimino codigo de informe laboratorio

--- app/src/main/java/com/marcos/fisikappmovil/InformeLaboratorio.java
+++ app/src/main/java/com/marcos/fisikappmovil/InformeLaboratorio.java
@@ -13,27 +13,5 @@ public class InformeLaboratorio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informe_laboratorio);
 
-        // Botón para regresar
-        ImageView btnBack = findViewById(R.id.btnBack);
-        if (btnBack != null) {
-            btnBack.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
-        }
-
-        // Botón finalizar (simulación de guardado)
-        View btnFinalizar = findViewById(R.id.btnFinalizar);
-        if (btnFinalizar != null) {
-            btnFinalizar.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(InformeLaboratorio.this, "Informe guardado correctamente", Toast.LENGTH_SHORT).show();
-                    finish(); // Regresa al menú principal
-                }
-            });
-        }
     }
 }
