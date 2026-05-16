bc24dd13e25f6f221d830423bc603db8e68af263
john salazar <johnfsala@gmail.com>
2026-05-16 09:34:06 -0500

    ppp

--- app/src/main/java/com/marcos/fisikappmovil/api/RetrofitClient.java
+++ app/src/main/java/com/marcos/fisikappmovil/api/RetrofitClient.java
@@ -3,6 +3,7 @@ package com.marcos.fisikappmovil.api;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
 public class RetrofitClient {
     private static Retrofit retrofit = null;
     private static final String BASE_URL = "http://10.0.2.2:8000/";
